89fbd0a3b6ce280e32c1486288cbc5ce0ac1de3f
Gustavo Moura <gustavomourasm@gmail.com>
2025-01-09 16:19:24 -0300

    vai

--- src/config/RepositoriesList.config.ts
+++ src/config/RepositoriesList.config.ts
@@ -3,6 +3,24 @@ import { Repositories } from "@/types/repositories.type";
 export const RepositoriesList : Repositories[] = [
     {
         id: 1,
+        title: "Sharly AI",
+        image: "/repositories/sharly.png",
+        description: "Sharly AI é um site que utiliza inteligência artificial para permitir conversas interativas com documentos PDF. Construído com o Framework Bootstrap, o site oferece uma interface responsiva e moderna, facilitando o upload de PDFs e a extração de informações de forma intuitiva e eficiente.",
+        technologies: ["/repositories/React-Dark.svg", "/repositories/NextJS-Dark.svg", "/repositories/Bootstrap.svg","/repositories/HTML.svg","/repositories/CSS.svg"],
+        path: "/repositories",
+        href: "https://sharly.ai"
+    },
+    {
+        id: 2,
+        title: "Rifaweb",
+        image: "/repositories/rifaweb.png",
+        description: "RifaWeb é uma plataforma de rifas digitais que torna a criação e a gestão de rifas simples e acessível. Desenvolvido com o Framework Bootstrap, o site oferece uma interface responsiva e amigável, permitindo que os usuários participem de rifas online de forma segura e prática.",
+        technologies: ["/repositories/React-Dark.svg", "/repositories/NextJS-Dark.svg", "/repositories/TailwindCSS-Dark.svg", "/repositories/TypeScript.svg"],
+        path: "/repositories",
+        href: "https://rifaweb.com"
+    },
+    {
+        id: 3,
         title: "Portfolio",
         image: "/repositories/Portfolio-wide-light.png",
         description: "Este é o meu site portfolio, site desenvolvido em React com o NextJS 15. O site demonstra todas as minhas habilidades, quem sou eu, meus contatos e meus repositórios.",
@@ -10,22 +28,4 @@ export const RepositoriesList : Repositories[] = [
         path: "/repositories",
         href: "/"
     },
-    {
-        id: 2,
-        title: "Caravan",
-        image: "/repositories/Caravan.png",
-        description: "Site de Planos para Viagens. Em sua construção foi o utilizado do Framework Bootstrap.",
-        technologies: ["/repositories/Bootstrap.svg","/repositories/HTML.svg","/repositories/CSS.svg"],
-        path: "/repositories",
-        href: "https://caravanstrip.vercel.app/"
-    },
-    {
-        id: 3,
-        title: "React-clock",
-        image: "/repositories/React-clock.png",
-        description: "Site de um relógio digital, feito para treinamento de habilidades adquiridas em react utilizando também o tailwind como framework.",
-        technologies: ["/repositories/React-Dark.svg", "/repositories/NextJS-Dark.svg", "/repositories/TailwindCSS-Dark.svg", "/repositories/TypeScript.svg"],
-        path: "/repositories",
-        href: "https://clockreact.vercel.app/"
-    }
 ]
